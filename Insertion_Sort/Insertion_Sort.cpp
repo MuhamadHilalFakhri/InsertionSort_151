d2e7ec046045c10aa7afd53b4a9789683c285f88
hilal555 <muhamadhilal04@gmail.com>
2023-03-24 10:39:40 +0700

    Prosedur input membuat banyak data yg akan dimasukkan

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -5,5 +5,28 @@ int arr[20];                            // Membuat array dengan panjang data 20
 int n;                                  // Membuat variabel inputan n
 
 void input() {					// Procedure Input
-	
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan Jumlah element Array
+		cin >> n;									  // Memanggil variable inputan n
+
+		if (n <= 20) {								  // Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";  // Menampilkan pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;											// Membuat jarak perbaris program
+	cout << "======================" << endl;					// Membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)								// Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke- " << (i + 1) << ": ";				// Memasukkan atau menginputkan nilai data n
+		cin >> arr[i];										// Menyimpan nilai data n kedalam array arr
+	}
+}
 		
