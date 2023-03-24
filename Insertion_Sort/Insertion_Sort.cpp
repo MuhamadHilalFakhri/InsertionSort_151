cd84411cf2dd13585332fd0cb0751f58f4bdd40b
hilal555 <muhamadhilal04@gmail.com>
2023-03-24 10:36:54 +0700

    menambahkan perintah input

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -5,6 +5,5 @@ int arr[20];                            // Membuat array dengan panjang data 20
 int n;                                  // Membuat variabel inputan n
 
 void input() {					// Procedure Input
-	while (true)
 	
 		
